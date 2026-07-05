3aa74bfc1e8e67d68bb2713d00b183d17cc8e344
Eduardo Augusto Lima Bueno <eduardoaugustolimabueno@outlook.com.br>
2026-07-05 20:28:56 -0300

    chore(api): migrate to railway infra

--- apps/api/src/infrastructure/db/config.ts
+++ apps/api/src/infrastructure/db/config.ts
@@ -3,23 +3,11 @@ import { Pool } from "pg";
 import { env } from "../../utils/env/index.ts";
 import * as schema from "./schema.ts";
 
-const cert = await Bun.file(".certs/postgre-certificate.pem").text();
-
-const dbUrl = new URL(env.DATABASE_URL);
-
 const pgPool = new Pool({
   connectionString: env.DATABASE_URL,
-  host: dbUrl.hostname,
-  port: parseInt(dbUrl.port, 10),
-  user: dbUrl.username,
-  password: dbUrl.password,
-  database: dbUrl.pathname.replace("/", ""),
-  ssl: {
-    cert,
-    ca: cert,
-    key: cert,
-    rejectUnauthorized: true,
-  },
+  max: 30,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
 });
 
 export const db = drizzle(pgPool, {
